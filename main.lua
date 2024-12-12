3d5c79bdf14ac680a84edf9ef8ef3af61c7330e8
1x2o17 <waughd91@gmail.com>
2024-12-13 10:40:27 +1300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,23 @@
 local ui = {}
 
 function ui:CreateWindow(params)
-    print('making window')
-    local window = Instance.new('Frame')
+    local window = Instance.new('ScreenGui')
+    window.Name = "734"
+
+    local main = Instance.new('Frame')
+    main.Name = "Main"
+    main.Size = UDim2.new(0.344, 0,0.422, 0)
+    main.Position = UDim2.new(0.328, 0,0.288, 0)
+    main.BackgroundColor3 = Color3.new(params.color)
+
+    main.Parent = window
+
+    
+
+    window.Parent = game.CoreGui
     return window
 end
 
 return ui
+
+-- {0, 525}, {0, 318}
